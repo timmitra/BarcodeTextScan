4c3954bf2afad7797c9e0736ce0f3f9cbb2a1e42
Tim Mitra <tim@it-guy.com>
2024-03-13 18:08:05 -0400

    Initial Commit

---
+++ BarcodeTextScan/BarcodeTextScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeTextScanApp.swift
+//  BarcodeTextScan
+//
+//  Created by Tim Mitra on 2024-03-13.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeTextScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarcodeTextScan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BarcodeTextScan
+//
+//  Created by Tim Mitra on 2024-03-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
