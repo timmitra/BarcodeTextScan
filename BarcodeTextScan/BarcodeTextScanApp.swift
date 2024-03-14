494805fb247fa7657796249b2202f92cd6615c86
Tim Mitra <tim@it-guy.com>
2024-03-13 21:51:26 -0400

    add stateObject to share vm with child views, and request scanner status

--- BarcodeTextScan/BarcodeTextScanApp.swift
+++ BarcodeTextScan/BarcodeTextScanApp.swift
@@ -9,9 +9,16 @@ import SwiftUI
 
 @main
 struct BarcodeTextScanApp: App {
+  
+  @StateObject private var vm = AppViewModel()
+  
     var body: some Scene {
         WindowGroup {
             ContentView()
+            .environmentObject(vm)
+            .task {
+              await vm.requestDataScannerAccessStatus()
+            }
         }
     }
 }
