08239f3dfdc9efc5cf81cf022cb969ec79e24ab9
Tim Mitra <tim@it-guy.com>
2024-03-13 22:31:05 -0400

    add a way to recognize the type

--- BarcodeTextScan/AppViewModel.swift
+++ BarcodeTextScan/AppViewModel.swift
@@ -9,6 +9,10 @@ import SwiftUI
 import AVKit
 import VisionKit
 
+enum ScanType {
+  case text, barcode
+}
+
 enum DataScannerAccessStatusType {
   case notDetermined
   case cameraAccessNotGranted
@@ -21,6 +25,14 @@ enum DataScannerAccessStatusType {
 final class AppViewModel: ObservableObject {
   
   @Published var dataScannerAccessStatus: DataScannerAccessStatusType = .notDetermined
+  @Published var recognizedItems: [RecognizedItem] = []
+  @Published var scanType: ScanType = .barcode
+  @Published var textContentType: DataScannerViewController.TextContentType?
+  @Published var recognizesMultipleItems = true
+  
+  private var recognizedDataType: DataScannerViewController.RecognizedDataType {
+    scanType == .barcode ? .barcode() : .text(textContentType: textContentType)
+  }
   
   private var isScannerAvailable: Bool {
     DataScannerViewController.isAvailable && DataScannerViewController.isSupported
